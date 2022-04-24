f5899e10bf0631416faa18a877e9fb3faeb2448f
Archibald <aff1218@ic.ac.uk>
2022-04-24 23:06:14 +0100

    Add tests with closed-form solution

--- toy_experiment/reparametrization.py
+++ toy_experiment/reparametrization.py
@@ -6,17 +6,15 @@ import jax.random as jrandom
 
 from constants import RAND_KEY
 from constants import NUM_BATCHES
-from toy_function import foo
+from toy_function import foo, linear_foo
 
 
 def rp_foo(mean, var, w, eps):
     rp_sample = mean + jnp.sqrt(var) * eps
     return foo(rp_sample, w)
 
-
 def get_rp_gradients(means, variances, w, N, num_inputs, key=RAND_KEY):
     epsilons = jrandom.normal(key=key, shape=(num_inputs,N))
-    # epsilons = jnp.tile(epsilons, (num_inputs, 1))
 
     mu_grad = jax.vmap(
         jax.vmap(grad(rp_foo, 0), in_axes=(0, 0, None, 0)), in_axes=(0, 0, None, 0)
@@ -45,6 +43,52 @@ def get_rp_grad_var(means, variances, w, N, num_inputs, num_batches=NUM_BATCHES)
                                     num_inputs,
                                     subkeys[i],
                                 )
+        mu_grads[i] = mu_grad.mean(axis=1)
+        var_grads[i] = var_grad.mean(axis=1)
+    
+    mu_var = mu_grads.var(axis=0)
+    var_var = var_grads.var(axis=0)
+    
+    return mu_var, var_var
+
+
+
+# Used for testing RP gradient estimator values
+
+def rp_linear_foo(mean, var, eps):
+    rp_sample = mean + jnp.sqrt(var) * eps
+    return linear_foo(rp_sample)
+    
+def get_rp_linear_foo_gradients(means, variances, N, num_inputs, key=RAND_KEY):
+    epsilons = jrandom.normal(key=key, shape=(num_inputs,N))
+
+    mu_grad = jax.vmap(
+        jax.vmap(grad(rp_linear_foo, 0), in_axes=(0, 0, 0)), in_axes=(0, 0, 0)
+    )
+    var_grad = jax.vmap(
+        jax.vmap(grad(rp_linear_foo, 1), in_axes=(0, 0, 0)), in_axes=(0, 0, 0)
+    )
+
+    mu_grads = mu_grad(means, variances, epsilons)
+    var_grads = var_grad(means, variances, epsilons)
+
+    return mu_grads, var_grads
+
+def get_rp_linear_foo_grad_var(means, variances, N, num_inputs, num_batches=NUM_BATCHES):
+    mu_grads = np.zeros((num_batches, num_inputs))
+    var_grads = np.zeros((num_batches, num_inputs))
+
+    subkeys = jrandom.split(RAND_KEY, num=num_batches)
+
+    for i in range(num_batches):
+        mu_grad, var_grad = get_rp_linear_foo_gradients(
+                                    means,
+                                    variances,
+                                    N,
+                                    num_inputs,
+                                    subkeys[i],
+                                )
+                                
         mu_grads[i] = mu_grad.mean(axis=1)
         var_grads[i] = var_grad.mean(axis=1)
     
--- toy_experiment/toy_function.py
+++ toy_experiment/toy_function.py
@@ -20,3 +20,23 @@ def mc_function(sample_var: int, xs, w: int, N: int, num_inputs: int):
     )
 
     return foo(mc_samples, w).mean(axis=0, keepdims=False)
+
+
+
+# Used for testing the gradient values against a closed-form solution
+def linear_foo(x):
+    # return jnp.full_like(x, 1)
+    return 3 * x + 4
+
+def mc_linear_foo(xs, variance, num_inputs, N):
+    means = jnp.array(xs, dtype=jnp.float32)
+    cov = jnp.eye(num_inputs, num_inputs, dtype=jnp.float32) * variance
+
+    mc_samples = jrandom.multivariate_normal(
+                        key=RAND_KEY,
+                        mean=means,
+                        cov=cov,
+                        shape=(N,)
+    )
+
+    return linear_foo(mc_samples).mean(axis=0, keepdims=False)
